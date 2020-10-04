108e570057595298ea958dc8a57f3df9012c7741
nilhahn <mheisler@outlook.de>
2020-10-04 14:50:58 +0200

    add cmdline parsing

---
+++ weather-gatherer-service/src/main/java/org/nilhahn/weather/service/CmdLineService.java
@@ -0,0 +1,54 @@
+package org.nilhahn.weather.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.cli.*;
+
+import java.util.Optional;
+
+@Slf4j
+public class CmdLineService {
+    private Options options = new Options();
+    private CommandLine cmd = null;
+
+    public enum Parameter {
+        APIKEY("apiKey"),
+        LOCATION("location");
+
+        private String parameter;
+
+        Parameter(final String parameter) {
+            this.parameter = parameter;
+        }
+
+        public String asString() {
+            return this.parameter;
+        }
+    }
+
+    public CmdLineService() {
+        Option apiKey = new Option("key", "apiKey", true, "open weather map api key");
+        Option location = new Option("l", "location", true, "open weather location");
+        apiKey.setRequired(true);
+        location.setRequired(true);
+        this.options.addOption(apiKey);
+        this.options.addOption(location);
+    }
+
+    public void parse(String[] args) {
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+
+        try {
+            cmd = parser.parse(this.options, args);
+        } catch (ParseException e) {
+            log.error(e.getMessage());
+            formatter.printHelp("weather-gatherer-service", this.options);
+
+            throw new Error("");
+        }
+    }
+
+    public Optional<String> getCmdLineParameter(Parameter parameter) {
+            return Optional.ofNullable(this.cmd.getOptionValue(parameter.asString()));
+    }
+}
