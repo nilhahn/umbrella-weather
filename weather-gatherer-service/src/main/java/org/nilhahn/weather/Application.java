f12bcc51e03a685a1aebbec0edfff5e16910de29
nilhahn <mheisler@outlook.de>
2020-10-07 20:59:42 +0200

    chore(Application) change in code style

--- weather-gatherer-service/src/main/java/org/nilhahn/weather/Application.java
+++ weather-gatherer-service/src/main/java/org/nilhahn/weather/Application.java
@@ -2,12 +2,9 @@ package org.nilhahn.weather;
 
 import lombok.extern.slf4j.Slf4j;
 import org.nilhahn.weather.connection.HttpConnector;
-import org.nilhahn.weather.model.WeatherResponse;
 import org.nilhahn.weather.service.CmdLineService;
 import org.nilhahn.weather.service.StorageService;
 import org.nilhahn.weather.service.WeatherService;
-
-import java.util.Optional;
 
 @Slf4j
 public class Application {
@@ -19,12 +16,11 @@ public class Application {
 
         WeatherService service = new WeatherService(new HttpConnector(),
                 cmdLineService.getCmdLineParameter(CmdLineService.Parameter.APIKEY)
-                .orElseThrow(() -> new RuntimeException("Api Key is missing"))
+                        .orElseThrow(() -> new RuntimeException("Api Key is missing"))
         );
-        Optional<WeatherResponse> response = service.getWeatherForLocation(
+        service.getWeatherForLocation(
                 cmdLineService.getCmdLineParameter(CmdLineService.Parameter.LOCATION)
-                        .orElseThrow(() -> new RuntimeException("Location is missing")));
-
-        response.ifPresent(storageService::store);
+                        .orElseThrow(() -> new RuntimeException("Location is missing")))
+                .ifPresent(storageService::store);
     }
 }
